bbe750804fd2b03f5eb27ad4343e71b89a572916
jdmcd-nald <120195133+jdmcd-nald@users.noreply.github.com>
2022-12-09 16:47:37 -0500

    Delete Polymer_Brush directory

--- Polymer_Brush/Constants.cpp
+++
@@ -1,7 +0,0 @@
-#ifdef _MSC_VER
-#define _CRT_SECURE_NO_WARNINGS
-#endif
-
-#include "Constants.h"
-
-
--- Polymer_Brush/EXWL.h
+++
@@ -1,9 +0,0 @@
-#ifndef EXWL_12_9_22_H
-#define EXWL_12_9_22_H
-
-void pseudowl();
-int histflat(int imin, int imax, double ratio);
-void accessiblelevels();
-void recombine(double counts);
-
-#endif
